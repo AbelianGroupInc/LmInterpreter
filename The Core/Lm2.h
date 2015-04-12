7e0924efa0a397d37464a2c25c0844274e1473c2
Alexey Bandura <tenrakyshka@gmail.com>
2015-04-13 00:09:13 +0300

    Updated LM2

--- The Core/Lm2.h
+++ The Core/Lm2.h
@@ -9,19 +9,24 @@
 
 class Lm2 : public Lm{
 public:
-	Lm2() :current_address(0), memory(){}
+	Lm2() :current_address(0), memory(),cmp_op(10){}
 	virtual void execute_the_program();
 	virtual void set_program(const std::vector<std::vector<int> > &program);
 	virtual std::vector<std::vector<int> > get_program()const;
 private:
+	
+	enum flags { EQUAL, N_EQUAL, LESS, GREATER, LESS_OR_EQUAL, GREATER_OR_EQUAL, INS_LESS, INS_GREATER, INS_LESS_OR_EQUAL, INS_GREATER_OR_EQUAL };
+	std::vector<bool> cmp_op;
 	int current_address;
 	Memory memory;
-
+	
 	void perform_arithmetic_operation(MemoryItem* (*func)(const MemoryItem*, const MemoryItem*));
-	void perform_comparison_operation(bool(*func)(const MemoryItem*, const MemoryItem*));
+	void perform_comparison_operation(flags flag);
 	void perform_input_operation(void(*func)(MemoryItem*&, const std::string));
 	void perform_output_operation(void(*func)(const MemoryItem*, const std::string));
 	void perform_assignment_operation();
+	void perfom_flag_determination();
+	void perform_goto();
 
 	int get_value_operand(int position_in_memory, int number_of_operands);
 	int get_address_operand(int position_in_memory, int number_of_operands);

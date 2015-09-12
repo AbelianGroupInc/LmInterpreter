c44be76539a969ba6798c9616c094dc59f9725b3
AndrewEremeew <shaylin3@gmail.com>
2015-09-12 14:30:37 +0300

    Updated UML Diagram

--- Windows - console/Command.h
+++ Windows - console/Command.h
@@ -5,11 +5,6 @@
 #include "MemoryItem.h"
 
 class Command :public MemoryItem{
-public:
-	virtual std::vector<int>& get() _NOEXCEPT = 0;
-	//Getting a command
-	virtual int get_value()const _NOEXCEPT = 0;
-	//Getting a number of a command
 };
 
 #endif
